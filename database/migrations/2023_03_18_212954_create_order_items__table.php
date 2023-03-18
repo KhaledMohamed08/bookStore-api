f6936b5e6b49783158ec34a7c64d6cc2d97d41d6
marina refat <marinarefat0@gmail.com>
2023-03-18 23:43:00 +0200

    database migration

---
+++ database/migrations/2023_03_18_212954_create_order_items__table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_items_', function (Blueprint $table) {
+            $table->id();
+            $table->decimal("price");
+            $table->integer("quantity");
+            $table->foreignId('order_id')
+            ->constrained('orders');
+            $table->foreignId('book_id')
+            ->constrained('books');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_items_');
+    }
+};
