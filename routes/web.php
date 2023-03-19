6e7f1b838e51f8da8079a3bf6d2933851a2311c3
KhaledMohamed08 <khaledmohamed0896@gmail.com>
2023-03-19 23:40:05 +0200

    fix cors

--- routes/web.php
+++ routes/web.php
@@ -21,5 +21,11 @@ Route::get('/', function () {
 // Auth::routes();
 Route::resource('writer',\App\Http\Controllers\WriterController::class);
 Route::resource('user',\App\Http\Controllers\UserController::class);
+Route::resource('books',\App\Http\Controllers\BookController::class);
+Route::resource('orders',App\Http\Controllers\OrderController::class);
+Route::resource('categories',App\Http\Controllers\CategoriesController::class);
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
 Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
