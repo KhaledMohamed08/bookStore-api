17d45df8d64010b46114dddf646d285da159dcea
Mahmoud Magdy <103686429+Mahmoud-Magdy17@users.noreply.github.com>
2023-03-19 18:20:08 +0200

    adding crud operations for user and writer tables

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,7 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
+// Auth::routes();
+Route::resource('writer',\App\Http\Controllers\WriterController::class);
+Route::resource('user',\App\Http\Controllers\UserController::class);
 Auth::routes();
-
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
